0ea14bb922dd7f8a9350e842570da914d0285978
SA2004MA <144704930+SA2004MA@users.noreply.github.com>
2023-09-11 21:05:22 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,12 @@
+# recursion function for factorial
+
+def fact_rec(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact_rec(n-1)
+
+number = 5
+res= fact_rec(number)
+
+print("the Factorial of {} is {}.".format(number,res))
